62d5fb84ae5af4ea892f8b3f47179516d9b43d6b
Magurolab <poon.@Poonnarats-MacBook-Pro.local>
2018-03-11 20:53:02 +0700

    messing up the front-end

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
   render() {
@@ -11,7 +12,8 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          axios.get('http://localhost:8080/greeting')
+
         </p>
       </div>
     );
