55819fa9b50e0786dc6dba560f8a6b540fe63e32
Magurolab <poon.@Poonnarats-MacBook-Pro.local>
2018-03-12 13:15:26 +0700

    hello world is working

---
+++ back-end/src/main/java/hard/string/configurations/CorsConfigurations.java
@@ -0,0 +1,24 @@
+package hard.string.controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class CorsConfigurations {
+    @Bean
+    public WebMvcConfigurer CORSConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                    .allowCredentials(true)
+                    .allowedMethods("*")
+                    .allowedHeaders("*")
+                    .allowedOrigins("http://localhost:3000");
+            }
+        };
+    }
+}
