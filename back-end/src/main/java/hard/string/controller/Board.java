1402f21fd6753b6bc60a910c1f135f886a4901a2
Atidhaya <secrettears53410@gmail.com>
2018-03-24 14:32:08 +0700

    before merge

--- back-end/src/main/java/hard/string/controller/Board.java
+++ back-end/src/main/java/hard/string/controller/Board.java
@@ -3,11 +3,10 @@ package hard.string.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * Created by Teama on 3/17/2018.
+ * Created by Teama on 3/24/2018.
  */
 
 @RestController
-public class GameController {
-
+public class Board {
 
 }
---
+++ back-end/src/main/java/hard/string/controller/HandController.java
@@ -0,0 +1,18 @@
+package hard.string.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Teama on 3/17/2018.
+ */
+
+@RestController
+public class HandController {
+
+//    @RequestMapping(method = RequestMethod.POST)
+//    public ResponseEntity attack
+
+}
