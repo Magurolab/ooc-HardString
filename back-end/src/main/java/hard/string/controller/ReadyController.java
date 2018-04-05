ff073e0cf5bfb2169189978422ae15822dd787ad
Atidhaya <secrettears53410@gmail.com>
2018-04-06 00:55:43 +0700

    READY PLAYER ONE

--- back-end/src/main/java/hard/string/controller/ReadyController.java
+++ back-end/src/main/java/hard/string/controller/ReadyController.java
@@ -8,11 +8,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by Teama on 4/5/2018.
  */
 
+@RestController
 @RequestMapping("/ready")
 public class ReadyController {
 
